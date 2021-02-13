1cb0c923946db495c9c425e33b85c13a7432f14e
annamaria99 <ams58@hi.is>
2021-02-13 19:36:58 +0000

    Laga database fyrir heroku vol2

--- src/db.js
+++ src/db.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 const {
   DATABASE_URL: connectionString,
+  NODE_ENV: nodeEnv = 'development',
 } = process.env;
 
 if (!connectionString) {
@@ -12,7 +13,8 @@ if (!connectionString) {
   process.exit(1);
 }
 
-const pool = new pg.Pool({ connectionString });
+const ssl = nodeEnv !== 'development' ? { rejectUnauthorized: false } : false;
+const pool = new pg.Pool({ connectionString, ssl });
 
 pool.on('error', (err) => {
   console.error('Unexpected error on idle client', err);
